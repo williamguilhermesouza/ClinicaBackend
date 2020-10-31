7e718580c0a7ab91e0bb82475335a2921676d25d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-10-31 18:47:32 -0300

    first service programming

--- src/Pacientes/pacientes.controller.ts
+++ src/Pacientes/pacientes.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Put, Post, Delete, Param } from '@nestjs/common';
+import { PacientesService } from './pacientes.service';
 
 @Controller('pacientes')
-export class PacientesController {}
+export class PacientesController {
+    constructor(private readonly pacientesService: PacientesService) {}
+
+    @Get()
+    findAllPacientes(): string {
+        return this.pacientesService.findAll();
+    }
+
+    @Post('/create')
+    createPaciente(): string {
+        return this.pacientesService.create();
+    }
+
+    @Put(':id')
+    updatePaciente(@Param() id): string {
+        return this.pacientesService.update(id);
+    }
+
+    @Delete(':id')
+    deletePaciente(@Param() id): string {
+        return this.pacientesService.delete(id);
+    }
+
+    @Get(':id')
+    findOne(@Param() id): string {
+        return this.pacientesService.findOne(id);
+    }
+
+}
--- src/Pacientes/pacientes.service.ts
+++ src/Pacientes/pacientes.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PacientesService {}
+export class PacientesService {
+    findAll(): string {
+        return 'findall';
+    }
+
+    create(): string {
+        return 'create';
+    }
+
+    update(id: number): string {
+        return 'update';
+    }
+
+    delete(id: number): string {
+        return 'delete';
+    }
+
+    findOne(id: number): string {
+        return `${id}`;
+    }
+
+}
